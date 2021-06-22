657db4e089fa9db725c354524f401fad38fae1a4
Ensar Emir EROL <ensaremir.erol99@gmail.com>
2021-06-22 14:14:04 +0300

    Fixed bug about memory allocation

--- tests/arithmeticBigInt8.c
+++ tests/arithmeticBigInt8.c
@@ -4,10 +4,10 @@
 #include "bigint.h"
 
 int main(){
-    char *assert = "-2995";
-    char *str1 = "-3000";
+    char *assert = "1132424235345654752349384610003";
+    char *str1 = "1132424235345654752349284620000";
     BigInt *bigInt1 = strtobigInt(str1);
-    char *str2 = "5";
+    char *str2 = "99990003";
     BigInt *bigInt2 = strtobigInt(str2);
     addBigInt(bigInt1, bigInt2);
     char *out = calloc(sizeof (char), strlen(assert) + 1);
