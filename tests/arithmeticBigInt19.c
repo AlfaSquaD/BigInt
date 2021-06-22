06f6b1987c64d6054ea680eda5e8b29c9d99731c
OmerVeliIlhan <79760995+OmerVeliIlhan@users.noreply.github.com>
2021-06-22 17:44:52 +0300

    Create arithmeticBigInt19.c

---
+++ tests/arithmeticBigInt19.c
@@ -0,0 +1,17 @@
+#include "bigint.h"
+
+int main(){
+    char *assert = "5762820741723105310739551494798190047956871176108";
+    char *str1 = "15125134865135446546152165846123875463123412341234";
+    BigInt *bigInt1 = strtobigInt(str1);
+    char *str2 = "-9362314123412341235412614351325685415166541165126";
+    BigInt *bigInt2 = strtobigInt(str2);
+    addBigInt(bigInt1, bigInt2);
+    char *out = calloc(sizeof (char), strlen(assert) + 1);
+    freopen("/dev/null", "a", stdout);
+    setbuf(stdout, out);
+    printBigInt(bigInt1);
+    freeBigInt(bigInt1);
+    freeBigInt(bigInt2);
+    return strcmp(out, assert);
+}
