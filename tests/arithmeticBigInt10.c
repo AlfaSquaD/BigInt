d8584ca78aa22fb037047236a6909aa48840f549
OmerVeliIlhan <79760995+OmerVeliIlhan@users.noreply.github.com>
2021-06-22 15:33:13 +0300

    Create arithmeticBigInt10.c

---
+++ tests/arithmeticBigInt10.c
@@ -0,0 +1,17 @@
+#include "bigint.h"
+
+int main(){
+    char *assert = "1014778275276475807973680980278198381989016800863058803387497635163125";
+    char *str1 = "62354652135241684561326854134685125134865135446546152165846123875463";
+    BigInt *bigInt1 = strtobigInt(str1);
+    char *str2 = "952423623141234123412354126143513256854151665416512651221651511287662";
+    BigInt *bigInt2 = strtobigInt(str2);
+    addBigInt(bigInt1, bigInt2);
+    char *out = calloc(sizeof (char), strlen(assert) + 1);
+    freopen("/dev/null", "a", stdout);
+    setbuf(stdout, out);
+    printBigInt(bigInt1);
+    freeBigInt(bigInt1);
+    freeBigInt(bigInt2);
+    return strcmp(out, assert);
+}
