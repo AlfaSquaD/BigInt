95698b0288506e5a56bf02b79da3fed4a9ff0ba0
Omer Veli İlhan <79760995+OmerVeliIlhan@users.noreply.github.com>
2021-06-22 18:37:33 +0300

    Create arithmeticBigInt23.c

---
+++ tests/arithmeticBigInt23.c
@@ -0,0 +1,17 @@
+#include "bigint.h"
+
+int main(){
+    char *assert = "10000000002";
+    char *str1 = "1";
+    BigInt *bigInt1 = strtobigInt(str1);
+    char *str2 = "-9999999999";
+    BigInt *bigInt2 = strtobigInt(str2);
+    subBigInt(bigInt1, bigInt2);
+    char *out = calloc(sizeof (char), strlen(assert) + 1);
+    freopen("/dev/null", "a", stdout);
+    setbuf(stdout, out);
+    printBigInt(bigInt1);
+    freeBigInt(bigInt1);
+    freeBigInt(bigInt2);
+    return strcmp(out, assert);
+}
