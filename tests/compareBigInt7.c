58b24bcb1eb8c6269affa060db2b0bdac5ac23bf
Omer Veli İlhan <79760995+OmerVeliIlhan@users.noreply.github.com>
2021-06-22 20:21:33 +0300

    Create compareBigInt7.c

---
+++ tests/compareBigInt7.c
@@ -0,0 +1,10 @@
+#include "bigint.h"
+
+int main(){
+    char *assert = "-1";
+    char *str1 = "-6485645783452123";
+    BigInt *bigInt1 = strtobigInt(str1);
+    char *str2 = "-64856457897864869789646456748978564674896787896786534545345633452123";
+    BigInt *bigInt2 = strtobigInt(str2);
+    if(compare(bigInt1, bigInt2) == 1) return 0;
+}
